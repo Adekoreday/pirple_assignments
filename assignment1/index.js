7e76bd5ceb71de5e4237876422caa67562bd7e2f
Adekoreday <khord4eng@gmail.com>
2019-01-04 11:26:57 -0800

    index.js modified

--- assignment1/index.js
+++ assignment1/index.js
@@ -8,14 +8,19 @@ var parsedurl = url.parse(req.url,true);
 path = parsedurl.pathname;
 //using regex to trimm out my url path
 var trimmedpath = path.replace(/^\/+|\/+$/g, '');
-
+var queryStringObject = parsedurl.query;
+//get the query string 
+var querymsg =  JSON.stringify(queryStringObject);
+// the route request handler
 var handlerchoosen = typeof(router[trimmedpath]) !== 'undefined' ? router[trimmedpath] : router.default;
+// the handler function call implementation
 handlerchoosen((statuscode,object1)=>{
     var message = typeof(object1)=='object' ? object1 : {};
     var payloadString = JSON.stringify(object1);
     res.setHeader('Content-Type','application/json');
     res.writeHead(statuscode);
     res.write(trimmedpath);
+    res.write(querymsg);
     res.end(payloadString);
     console.log("Returning this response: ", statuscode, payloadString);
 
@@ -25,7 +30,7 @@ handlerchoosen((statuscode,object1)=>{
 server.listen(80,()=>{
 console.log('hello client');
 });
-
+// handllers
 var handlers= {
 };
 
@@ -34,7 +39,10 @@ handlers.default= (callback)=>{
 callback(406,{'name':'my name is Adeyemi-Adekorede am glad to be on pirple'});
 
 }
-
+handlers.goodbye=(callback)=>{
+    callback(406,{'name':'Goodbye!!! glad to be on pirple'});
+}
 var router={
-    'default':handlers.default
+    'default':handlers.default,
+    'bye':handlers.goodbye
 };
